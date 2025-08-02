8d9f097315ea0bd0fa0e4ddd9748f0a1c64e1199
ormon <armnvrma10@gmail.com>
2025-08-02 18:57:04 +0530

    handler setup done

--- src/handlers.rs
+++ src/handlers.rs
@@ -12,11 +12,17 @@ pub async fn get_depth(orderbook : Data<Arc<Mutex<Orderbook>>>) -> impl Responde
 }
 
 #[post("/order")]
-pub async fn create_order() -> impl Responder {
-    
+pub async fn create_order(orderbook : Data<Arc<Mutex<Orderbook>>>, order: Json<CreateOrderInput>) -> impl Responder {
+    let mut orderbook = orderbook.lock().unwrap();
+    let orderbook = orderbook.create_order(order.0);
+
+    HttpResponse::Ok().json(orderbook)
 }
 
 #[delete("/order")]
-pub async fn delete_order() -> impl Responder {
-    "Hello, world!"
+pub async fn delete_order(orderbook : Data<Arc<Mutex<Orderbook>>>, order : Json<DeleteOrderInput>) -> impl Responder {
+    let mut orderbook = orderbook.lock().unwrap();
+    let orderbook = orderbook.delete_order(order.0);
+
+    HttpResponse::Ok().json(orderbook)
 }
